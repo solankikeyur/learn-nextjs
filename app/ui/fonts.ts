ab6db7b0e668162fca68bacc01e8c1463b4a1333
keyursolanki7071 <keyur.solanki@arsenaltech.com>
2025-05-26 10:41:26 +0530

    Fix deployment issue

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
 import {Inter, Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({subsets: ['latin'], weight: "700"})
+export const lusitana = Lusitana({subsets: ['latin'], weight: "700", preload: false})
